fd1bdf9db71f8149b235eebcc54a6acdbb16917e
Lucas Toner <72283750+NannyBob@users.noreply.github.com>
2021-08-02 14:42:52 +0100

    Added Aldous-Broder Generation Algorithm

--- Global.gd
+++ Global.gd
@@ -3,8 +3,8 @@ extends Node
 
 var Maze_Size:Vector2 = Vector2(20,20)
 var Scale:Vector2
-enum TRAVERSAL { Dijkstra,Best_First,A_Star }
-enum BUILDING {Kruskal,Depth_First}
+enum TRAVERSAL { Dijkstra,A_Star,Best_First }
+enum BUILDING {Kruskal,Depth_First,Aldous_Broder}
 var Building_Algo
 var Traversal_Algo_1
 var Traversal_Algo_2
